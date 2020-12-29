69f9576d4e5c9435989d5128a087f0b6c2dc9494
cristicretu <cristicretu38@gmail.com>
2020-12-29 12:24:19 +0200

    boi

---
+++ infoarena/cardinal/cardinal.cc
@@ -0,0 +1,26 @@
+/**
+ *    author: etohirse
+ *    created: 29.12.2020 11:49:05
+ **/
+#include <bits/stdc++.h>
+
+std::ifstream fin("cardinal.in");
+std::ofstream fout("cardinal.out");
+
+int main() {
+  int t;
+  fin >> t;
+  while (t--) {
+    int n;
+    fin >> n;
+    if (!n % 8)
+      for (int i = 1; i <= n / 8; ++i) fout << "URDLDLUR";
+    else if (n & 8 == 7) {
+      fout << "URDRDLU";
+      for (int i = 1; i <= (n - 7) / 8; ++i) fout << "RULDLDRU";
+    } else
+      fout << "IMPOSIBIL";
+    fout << '\n';
+  }
+  return 0;
+}
---
+++ infoarena/diferente/diferente.cpp
@@ -0,0 +1,13 @@
+/**
+ *    author: etohirse
+ *    created: 29.12.2020 12:07:08
+ **/
+#include <bits/stdc++.h>
+
+std::ifstream fin("diferente.in");
+std::ofstream fout("diferente.out");
+
+int main() {
+  int n, k;
+  return 0;
+}

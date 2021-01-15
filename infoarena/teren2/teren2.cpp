1d38b3c152d519f7b76dfe5c0a73680dc93d83e2
cristicretu <cristicretu38@gmail.com>
2021-01-15 19:20:54 +0200

    ww

--- infoarena/teren2/teren2.cpp
+++ infoarena/teren2/teren2.cpp
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 
+typedef long long ll;
+
 std::ifstream fin("teren2.in");
 std::ofstream fout("teren2.out");
 
+const int mxn = 1000005;
+
+std::pair<int, int> v[mxn];
+
 int main() {
-  int n, d;
+  int n;
+  ll d;
   fin >> n >> d;
 
-  std::vector<std::pair<int, int>> v(n);
-  for (auto& i : v) fin >> i.first >> i.second;
+  for (int i = 1; i <= n; i++) fin >> v[i].first >> v[i].second;
+  v[n + 1] = v[1];
+  v[0] = v[n + 1];
 
-  int mutare = 30005, poz;
-  for (int i = 0; i < n; ++i)
-    if (abs(v[i].first) + abs(v[i].second) < mutare)
-      poz = i, mutare = abs(v[i].first) + abs(v[i].second);
-
-  if (v[poz].first > v[poz].second) {
-  }
-
-  v.push_back({v[0].first, v[0].second});
-
-  long arie = 0;
-  for (int i = 0; i < n; ++i)
+  ll arie = 0;
+  for (int i = 1; i <= n; i++)
     arie += v[i].first * v[i + 1].second - v[i + 1].first * v[i].second;
+  arie = abs(arie);
+  ll ans = arie;
+  long long dx[4] = {0, 0, d, -d}, dy[4] = {d, -d, 0, 0};
+  for (int i = 1; i <= n; i++)
+    for (int k = 0; k < 4; k++) {
+      ll x = v[i].first + dx[k], y = v[i].second + dy[k], temp = arie;
+      temp -= (v[i - 1].first * v[i].second - v[i].first * v[i - 1].second) +
+              (v[i].first * v[i + 1].second - v[i + 1].first * v[i].second);
 
+      temp += (v[i - 1].first * y - x * v[i - 1].second) +
+              (x * v[i + 1].second - v[i + 1].first * y);
+      ans = std::max(ans, 1LL * abs(temp));
+    }
+  fout << std::fixed << std::setprecision(6) << (double)ans / 2.0;
   return 0;
 }

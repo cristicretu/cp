9d053bbd6cf49c241907c67bd526bbba7248fa5d
cristicretu <cristicretu38@gmail.com>
2021-02-22 19:59:08 +0200

    m

--- infoarena/taxa/taxa.cpp
+++ infoarena/taxa/taxa.cpp
@@ -1,8 +1,8 @@
 #include <fstream>
 #include <queue>
 
-std::ifstream fin("taxa.in");
-std::ofstream fout("taxa.out");
+std::fstream fin("taxa.in", std::ios::in);
+std::fstream fout("taxa.out", std::ios::out);
 
 static const int dx[] = {0, -1, 0, 1, 0, 1, 1, -1, -1}, oo = 1e9,
                  dy[] = {0, 0, 1, 0, -1, 1, -1, 1, -1}, mxn = 1e3 + 1;
@@ -11,11 +11,20 @@ std::queue<std::pair<int, int>> cords;
 
 int n, m, l0, c0, lf, cf, a[mxn][mxn], mat[mxn][mxn];
 
-inline bool ok(int i, int j) { return i && j && i <= n && j <= m; }
+int main() {
+  fin >> n >> m >> l0 >> c0 >> lf >> cf;
 
-inline int Cost(int x, int y) { return x * y * (x != y); }
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      fin >> a[i][j];
+      mat[i][j] = oo;
+    }
+  }
 
-inline void Lee() {
+  auto ok = [&](int i, int j) -> bool { return i && j && i <= n && j <= m; };
+
+  auto Cost = [&](int x, int y) -> int { return x * y * (x != y); };
+
   cords.push({l0, c0});
   mat[l0][c0] = 0;
 
@@ -32,25 +41,7 @@ inline void Lee() {
       }
     }
   }
-}
-
-int main() {
-  fin >> n >> m >> l0 >> c0 >> lf >> cf;
-
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= m; ++j) {
-      fin >> a[i][j];
-      mat[i][j] = oo;
-    }
-  }
-
-  Lee();
 
   fout << mat[lf][cf];
-  // for (int i = 1; i <= n; ++i, fout << std::endl) {
-  //   for (int j = 1; j <= m; ++j) {
-  //     fout << mat[i][j] << ' ';
-  //   }
-  // }
   return 0;
 }

fb8d18cc9b00f3409889cec6873c5c94273a6ef6
cristicretu <cristicretu38@gmail.com>
2021-03-06 18:41:10 +0200

    m

--- OJI/random.cpp
+++ OJI/random.cpp
@@ -1,39 +1,72 @@
-#include <algorithm>
-#include <iostream>
+#include <fstream>
 
-static const int mxn = 1e5;
+using namespace std;
 
-int n, a[1 + mxn];
+ifstream fin("patrate.in");
+ofstream fout("patrate.out");
 
-void solve(int lb, int rb) {
-  int i = lb, j = rb, piv = a[lb];
-
-  while (i <= j) {
-    while (i < rb && a[i] < piv) i++;
-    while (j > lb && a[j] > piv) j--;
-
-    if (i <= j) {
-      std::swap(a[i], a[j]);
-      i++;
-      j--;
-    }
-  }
-
-  if (lb < j) solve(lb, j);
-  if (rb > i) solve(i, rb);
-}
+int a[201][201], n, m;
 
 int main() {
-  std::cin >> n;
-  for (int i = 1; i <= n; ++i) {
-    std::cin >> a[i];
-  }
+  fin >> n >> m;
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) fin >> a[i][j];
 
-  solve(1, n);
+  int SumaMinima = 0, pi = 0, pj = 0, situatie = 0;
 
-  for (int i = 1; i <= n; ++i) {
-    std::cout << a[i] << ' ';
-  }
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) {
+      int sumaSus = 0, sumaJos = 0, sumaStanga = 0, sumaDreapta = 0;
 
+      for (int l = 1; l < i; l++) sumaSus = sumaSus + a[l][j];
+
+      for (int l = i + 1; l <= n; l++) sumaJos = sumaJos + a[l][j];
+
+      for (int c = 1; c < j; c++) sumaStanga = sumaStanga + a[i][c];
+
+      for (int c = j + 1; c <= m; c++) sumaDreapta = sumaDreapta + a[i][c];
+
+      if (sumaJos + sumaStanga + a[i][j] > SumaMinima) {
+        SumaMinima = sumaJos + sumaStanga + a[i][j];
+        pi = i;
+        pj = j;
+        situatie = 1;
+      }
+
+      if (sumaSus + sumaStanga + a[i][j] > SumaMinima) {
+        SumaMinima = sumaSus + sumaStanga + a[i][j];
+        pi = i;
+        pj = j;
+        situatie = 2;
+      }
+
+      if (sumaJos + sumaDreapta + a[i][j] > SumaMinima) {
+        SumaMinima = sumaJos + sumaDreapta + a[i][j];
+        pi = i;
+        pj = j;
+        situatie = 3;
+      }
+
+      if (sumaSus + sumaDreapta + a[i][j] > SumaMinima) {
+        SumaMinima = sumaSus + sumaDreapta + a[i][j];
+        pi = i;
+        pj = j;
+        situatie = 4;
+      }
+    }
+
+  fout << SumaMinima << '\n' << pi << " " << pj << " ";
+
+  if (situatie == 1)  // L-ul e orientat in stanga si in jos
+    fout << -pj << " " << -(n - pi + 1) << '\n';
+
+  else if (situatie == 2)  // L-ul e orientat in stanga si in sus
+    fout << -pj << " " << pi << "\n";
+
+  else if (situatie == 3)  // L-ul e orientat in dreapta si in jos
+    fout << (m - pj + 1) << " " << -(n - pi + 1) << "\n";
+
+  else  // L-ul e orientat in dreapta si in sus
+    fout << (m - pj + 1) << " " << pi << "\n";
   return 0;
 }

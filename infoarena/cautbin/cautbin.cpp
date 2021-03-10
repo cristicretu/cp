0d2dc1a3dfc23f32a0c80b35446d1ace85c2bcd7
cristicretu <cristicretu38@gmail.com>
2021-03-10 18:29:01 +0200

    em

--- infoarena/cautbin/cautbin.cpp
+++ infoarena/cautbin/cautbin.cpp
@@ -3,30 +3,53 @@
 std::ifstream fin("cautbin.in");
 std::ofstream fout("cautbin.out");
 
+static const int mxn = 1e5;
+
+int n, v[1 + mxn], m;
+
 int main() {
-  using namespace std;
-
-  int n, m;
   fin >> n;
-
-  vector<int> v(n);
-  for (auto &i : v) fin >> i;
-  sort(v.begin(), v.end());
+  for (int i = 1; i <= n; ++i){
+    fin >> v[i];
+  }
 
   fin >> m;
-  for (int i = 0; i < m; ++i) {
-    int a, b;
+  for (int t = 1; t <= m; ++t){
+    int a, b, ans;
     fin >> a >> b;
+    if (a == 2){
+      int lb = 1, rb = n;
+      while (lb <= rb){
+        int mb = lb + (rb - lb) / 2;
 
-    if (a == 0) {
-      int ans = upper_bound(v.begin(), v.end(), b) - v.begin() - 1;
-      fout << (ans >= 0 and ans < n and v[ans] == b ? ans + 1 : -1) << '\n';
-    } else if (a == 1) {
-      int ans = lower_bound(v.begin(), v.end(), b + 1) - v.begin() - 1;
-      fout << ans + 1 << '\n';
-    } else if (a == 2) {
-      int ans = upper_bound(v.begin(), v.end(), b - 1) - v.begin();
-      fout << ans + 1 << '\n';
+        if (v[mb] < b){
+          lb = mb + 1;
+        } else {
+          rb = mb - 1;
+          ans = mb;
+        }
+      }
+
+      fout << ans << '\n';
+    } else {
+      int lb = 1, rb = n;
+      while (lb <= rb){
+        int mb = lb + (rb - lb) / 2;
+
+        if (v[mb] <= b){
+          ans = mb;
+          lb = mb + 1;
+        } else {
+          rb = mb - 1;
+        }
+      }
+
+      if (a == 0){
+        if (v[ans] == b) fout << ans << '\n';
+        else fout << "-1\n";
+      } else {
+        fout << ans << '\n';
+      }
     }
   }
   return 0;

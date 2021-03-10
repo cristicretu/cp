24915edf66bbb3a332ce4482da5d189cbd4c6689
cristicretu <cristicretu38@gmail.com>
2021-03-10 17:08:07 +0200

    m

--- infoarena/aria/aria.cpp
+++ infoarena/aria/aria.cpp
@@ -3,18 +3,27 @@
 std::ifstream fin("aria.in");
 std::ofstream fout("aria.out");
 
-int main() {
+
+int main(){
   int n;
   fin >> n;
 
   std::vector<std::pair<double, double>> v(n);
-
-  for (auto& i : v) fin >> i.first >> i.second;
+  for (auto &i : v){
+    fin >> i.first >> i.second;
+  }
   v.push_back({v[0].first, v[0].second});
-
+  
   double ans = 0;
-  for (int i = 0; i < n; ++i)
+  for (int i = 0; i < n; ++i){
     ans += v[i].first * v[i + 1].second - v[i + 1].first * v[i].second;
-  fout << std::fixed << std::setprecision(5) << ans / 2.0;
-  return 0;
+  }
+
+  ans /= 2.0;
+  if (ans == (int)ans){
+    fout << ans << '\n';
+  } else {
+    fout << std::fixed << std::setprecision(5) << ans << '\n';
+  }
+
 }

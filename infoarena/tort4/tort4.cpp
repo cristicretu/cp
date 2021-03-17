0161c7d79b665b2b9a9505aa5f8f6adf36a7914b
cristicretu <cristicretu38@gmail.com>
2021-03-17 09:06:41 +0200

    m

---
+++ infoarena/tort4/tort4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+std::fstream fin("tort4.in", std::ios::in);
+std::fstream fout("tort4.out", std::ios::out);
+
+static const int mxn = 2e5;
+
+int n, v[1 + mxn], sum, ans;
+bool fv[1 + 2 * mxn];
+
+int main(){
+	fin >> n;
+  for (int i = 1; i <= n; ++i){
+    fin >> v[i];
+  }
+  for (int i = n; i; --i){
+    sum += v[i];
+    fv[sum] = 1;
+  }
+  for (int i = 1; i < sum; ++i){
+    for (int j = i; j < sum && fv[j]; j += i){
+      ans++;
+    }
+  }
+  fout << ans << '\n';
+  return 0;
+}

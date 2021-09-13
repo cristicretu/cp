f4ef57a55462cccb0dbd79ee0fb097041d08730f
Cristi Cretu <crisemcr@gmail.com>
2021-09-13 14:17:04 +0300

    add dijkstra

--- infoarena/dijkstra/dijkstra.cpp
+++ infoarena/dijkstra/dijkstra.cpp
@@ -1,62 +1,71 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream>
 
-std::ifstream fin("dijkstra.in");
-std::ofstream fout("dijkstra.out");
+std::fstream fin("dijkstra.in", std::ios::in);
+std::fstream fout("dijkstra.out", std::ios::out);
 
-const int mxn = 5e4 + 5, inf = (1 << 30);
+const int mxn = 50001;
+const int INF = (1 << 30);
 
 int n, m;
-int d[mxn];
-
-bool inq[mxn];
-
-std::vector<std::pair<int, int>> g[mxn];
+int dist[mxn];
+bool inQ[mxn];
 
 struct cmp {
-  bool operator()(int x, int y) { return d[x] > d[y]; }
+  bool operator()(int x, int y) {
+    return dist[x] > dist[y];
+  }
 };
 
+std::vector<std::pair<int,int> > g[mxn];
 std::priority_queue<int, std::vector<int>, cmp> cords;
 
-void Dijkstra(int nod) {
-  for (int i = 1; i <= n; i++) d[i] = inf;
-
-  d[nod] = 0;
-
-  cords.push(nod);
-  inq[nod] = true;
+void Dijkstra(int node) {
+  dist[node] = 0;
+  cords.push(node);
+  inQ[node] = 1;
 
   while (!cords.empty()) {
-    int current = cords.top();
+    int newNode = cords.top();
     cords.pop();
+    inQ[newNode] = 0;
 
-    inq[current] = false;
-    for (size_t i = 0; i < g[current].size(); i++) {
-      int Vecin = g[current][i].first;
-      int Cost = g[current][i].second;
-      if (d[current] + Cost < d[Vecin]) {
-        d[Vecin] = d[current] + Cost;
-        if (inq[Vecin] == false) {
-          cords.push(Vecin);
-          inq[Vecin] = true;
+    for (auto vecin : g[newNode]) {
+      if (dist[newNode] + vecin.second < dist[vecin.first]) {
+        dist[vecin.first] = dist[newNode] + vecin.second;
+        if (!inQ[vecin.first]) {
+          cords.push(vecin.first);
+          inQ[vecin.first] = 1;
         }
       }
     }
   }
 }
 
-int main() {
+int main(){
   fin >> n >> m;
-  for (int i = 1; i <= m; ++i) {
-    int x, y, cost;
-    fin >> x >> y >> cost;
-    g[x].push_back({y, cost});
+
+  for (int i = 0; i < m; ++i) {
+    int x, y, c;
+    fin >> x >> y >> c;
+
+    g[x].push_back({y, c});
+  }
+
+  for (int i = 1; i <= n; ++i) {
+    dist[i] = INF;
   }
 
   Dijkstra(1);
 
   for (int i = 2; i <= n; ++i) {
-    fout << (d[i] != inf ? d[i] : 0) << ' ';
+    if (dist[i] != INF) {
+      fout << dist[i] << ' ';
+    } else {
+      fout << "0 ";
+    }
   }
   return 0;
 }
--- infoarena/heapuri/heapuri.cpp
+++ infoarena/heapuri/heapuri.cpp
@@ -5,24 +5,22 @@
 std::fstream fin("heapuri.in", std::ios::in);
 std::fstream fout("heapuri.out", std::ios::out);
 
-const int mxn = 10000;
+const int mxn = 200005; 
 
-int n, poz[2 * mxn + 5], idx;
+int n, poz[mxn], idx;
 std::set<int> heap;
 
 int main(){
   fin >> n;
+  int x, a;
   for (int i = 1; i <= n; ++i) {
-    int x;
     fin >> x;
 
     if (x == 1) {
-      int a;
       fin >> a;
       heap.insert(a);
       poz[++idx] = a;
     } else if (x == 2) {
-      int a;
       fin >> a;
       heap.erase(poz[a]);
     } else {

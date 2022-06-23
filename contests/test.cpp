a554154270be1e1ec5bd80e1b527186d0e3b82c1
Cristian Crețu <crisemcr@gmail.com>
2022-06-23 11:07:04 +0300

    feat: add bac

---
+++ contests/test.cpp
@@ -0,0 +1,84 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cpp
+ *
+ *    Description:  Test
+ *
+ *        Version:  1.0
+ *        Created:  06/23/2022 09:34:27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+using namespace std;
+
+
+int secventa(int n) {
+  int put = 1;
+  
+  int ans = n;
+  while (n % 100) {
+    if (n % 100 == 22) {
+      ans = ans - 2 * put;
+    }
+
+    put *= 10;
+
+    n /= 10;
+  }
+
+  return ans;
+}
+
+int a[11][11], n, m;
+
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
+int main() {
+  // cout << secventa(202233228);
+  cin >> m >> n;
+
+  for (int i = 1; i <= m; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      cin >> a[i][j];
+    }
+  }
+
+  int ans(0);
+
+  for (int i = 1; i <= m; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      int min = 11;
+      for (int k = 0; k < 4; ++k) {
+        if (a[i][j] <= a[i + dx[k]][j + dy[k]] && (
+              i + dx[k] >= 1 && i + dx[k] <= m &&
+              j + dy[k] >= 1 && j + dy[k] <= n
+              ))
+          min = a[i + dx[k]][j + dy[k]];
+      }
+      if (min > a[i][j]) ans += (min - a[i][j]), a[i][j] = min;
+    }
+  }
+
+  // sub 3
+  int x, y;
+  fin >> x >> y;
+
+
+  int a, last;
+  int ans(0);
+  while (fin >> a) {
+    if (a <= y && a >= x && a != last) {
+      ans += 1;
+    }
+  }
+}

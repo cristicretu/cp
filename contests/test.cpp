641e417515d5d117110e49af4b99405cfc317dde
Cristian Crețu <crisemcr@gmail.com>
2022-08-01 12:19:50 +0300

    feat: add mau subj

--- contests/test.cpp
+++ contests/test.cpp
@@ -16,8 +16,11 @@
  * =====================================================================================
  */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+ifstream fin("test.in");
 
 
 int secventa(int n) {
@@ -45,40 +48,59 @@ const int dy[4] = {0, 1, 0, -1};
 
 int main() {
   // cout << secventa(202233228);
-  cin >> m >> n;
+  // cin >> m >> n;
 
-  for (int i = 1; i <= m; ++i) {
-    for (int j = 1; j <= n; ++j) {
-      cin >> a[i][j];
+  int a, previous = 0, target = 0, remains = 0, l = 0, ans = 0;
+
+  while (fin >> a) {
+    if (a != previous && l != target + remains) {
+      l = 0;
+      remains = 0;
+    } else if (a != previous) {
+      remains += l;
+              ans = max(ans, l);
+
     }
+    target = a;
+    l += 1;
+    previous = a;
   }
+        ans = max(ans, l);
 
-  int ans(0);
+  cout << ans;
 
-  for (int i = 1; i <= m; ++i) {
-    for (int j = 1; j <= n; ++j) {
-      int min = 11;
-      for (int k = 0; k < 4; ++k) {
-        if (a[i][j] <= a[i + dx[k]][j + dy[k]] && (
-              i + dx[k] >= 1 && i + dx[k] <= m &&
-              j + dy[k] >= 1 && j + dy[k] <= n
-              ))
-          min = a[i + dx[k]][j + dy[k]];
-      }
-      if (min > a[i][j]) ans += (min - a[i][j]), a[i][j] = min;
-    }
-  }
+  // for (int i = 1; i <= m; ++i) {
+  //   for (int j = 1; j <= n; ++j) {
+  //     cin >> a[i][j];
+  //   }
+  // }
+
+  // int ans(0);
+
+  // for (int i = 1; i <= m; ++i) {
+  //   for (int j = 1; j <= n; ++j) {
+  //     int min = 11;
+  //     for (int k = 0; k < 4; ++k) {
+  //       if (a[i][j] <= a[i + dx[k]][j + dy[k]] && (
+  //             i + dx[k] >= 1 && i + dx[k] <= m &&
+  //             j + dy[k] >= 1 && j + dy[k] <= n
+  //             ))
+  //         min = a[i + dx[k]][j + dy[k]];
+  //     }
+  //     if (min > a[i][j]) ans += (min - a[i][j]), a[i][j] = min;
+  //   }
+  // }
 
   // sub 3
-  int x, y;
-  fin >> x >> y;
+  // int x, y;
+  // fin >> x >> y;
 
 
-  int a, last;
-  int ans(0);
-  while (fin >> a) {
-    if (a <= y && a >= x && a != last) {
-      ans += 1;
-    }
-  }
+  // int a, last;
+  // int ans(0);
+  // while (fin >> a) {
+  //   if (a <= y && a >= x && a != last) {
+  //     ans += 1;
+  //   }
+  // }
 }

cfeab11bbfe6e81055e5463dbde70bbdd4372f1d
Cristian Cretu <crisemcr@gmail.com>
2023-03-06 17:17:14 +0200

    feat

--- contests/OJI/sepi/platforma.cpp
+++ contests/OJI/sepi/platforma.cpp
@@ -15,7 +15,7 @@ static const int N = 15000;
 unsigned short opt;
 int n;
 ll v[N + 1];
-int dp[N];
+ll dp[N];
 
 bool solve1();
 void solve2();
@@ -83,46 +83,24 @@ bool solve1() {
 void solve2() {
   fin >> n;
   int lim = n / 2 + n % 2;
-  /*
-  ideea e ca tu la fiecare pas, daca iti este par, nu ai cum sa generezei un sir
-  nou asadar, numarul tau de variante e la fel ca si la i-1 daca pasul iti este
-  impar, atunci tu mai ai o secventa noua, in care fie ii lasi numarul lui la
-  final, fie face parte dintr-o treapta care incepe la i si se termina la 2*i-1
-  */
+  ll ans = 1;
 
-  dp[2] = 1;
-  for (int i = 3; i <= n; ++i) {
-    if (i % 2) {
-      dp[i] = 1;
-      dp[2 * i] += 1 + dp[i];
+  dp[1] = 1;
+  for (int i = 2; i < lim; ++i) {
+    if (i % 2 == 0) {
+      dp[i] = dp[i - 1];
     } else {
-      dp[i] += dp[i - 1];
+      dp[i] = dp[i / 2] + dp[i - 1];
     }
+    
+    dp[2 * i] += 1;
+    ans += dp[i];
   }
 
-  cout << dp[n];
-  for (int i = 1; i <= n; ++i) {
+  for (int i = 2; i <= lim; ++i) {
     cout << dp[i] << ' ';
   }
-
-  // for (int j = 1; j <= n; j++) {
-  // 		ans += dp[n][j]; // suma tuturor numarului de variante la ultimul
-  // pas impar
-  // }
-
-  // for (int i = 2; i <= lim; ++i) {
-  // 		dp[i] = 1;
-  // 		for (int j = 2; j <= i / 2 + i % 2; ++j) {
-  // 				dp[i] += dp[i * j];
-  // 		}
-  // }
-  //
-  // ll ans = 0;
-  // for (int i = 2; i <= lim; ++i) {
-  // 		ans += dp[i];
-  // }
-  //
-  // fout << ans;
+  fout << ans + 1;
 }
 
 void solve3() {}
--- infoarena/dijkstra/dijkstra.cpp
+++ infoarena/dijkstra/dijkstra.cpp
@@ -11,12 +11,12 @@ ofstream fout("dijkstra.out", std::ios::out);
 
 const int INF = 0x3f3f3f3f;
 
-using PI = pair<int,int>;
+using PI = pair<int, int>;
 using VI = vector<int>;
 using VP = vector<PI>;
 using VVP = vector<VP>;
 
-int n, m;
+unsigned n, m;
 VVP G;
 VI dist;
 
@@ -28,21 +28,34 @@ int main() {
   read();
   dijkstra(1, dist);
   write();
-  return 0; 
+  return 0;
+}
+
+void read() {
+  fin >> n >> m;
+  G = VVP(n + 1);
+  
+  int x, y, w;
+  while (m--) {
+    fin >> x >> y >> w; 
+    G[x].emplace_back(y, w);
+  }
 }
 
 void dijkstra(int x, VI& d) {
   priority_queue<PI, VP, greater<PI> > Q;
+  Q.emplace(0, x);
+  
   d = VI(n + 1, INF);
   d[x] = 0;
-
+  
   Q.emplace(0, x);
   while (!Q.empty()) {
     auto [dx, x] = Q.top();
     Q.pop();
-
+  
     if (dx > d[x]) continue;
-
+  
     for (auto node : G[x]) {
       auto [y, w] = node;
       if (d[y] > dx + w) {
@@ -53,23 +66,12 @@ void dijkstra(int x, VI& d) {
   }
 }
 
-void read() {
-  fin >> n >> m;
-  G = VVP(n + 1);
-
-  int x, y, w;
-  while (m--) {
-    fin >> x >> y >> w;
-    G[x].emplace_back(y, w);
-  }
-}
-
 void write() {
-  for (int i = 2; i <= n; ++i) {
-    if (dist[i] != INF) {
-      fout << dist[i] << ' ';
+  for (unsigned i = 2; i <= n; ++i) {
+    if (dist[i] == INF) {
+      fout << "0 ";
     } else {
-      fout << 0 << ' ';
+      fout << dist[i] << ' ';
     }
   }
   fout << '\n';
